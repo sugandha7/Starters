4141e5b37188aacb5c6f6cb8723d8a1f6f1a1149
Sugandha Goel <sugandhagoel7@gmail.com>
2013-05-31 13:40:48 +0530

    Update printmax.py

--- printmax.py
+++ printmax.py
@@ -2,7 +2,7 @@ def printmax(a,b):
 	''' Prints the max of two numbers.
 	
 	 The numbers must be integers.'''
-	x=int(a) #Convert the no. to intetger if it is not.
+	x=int(a) #Convert to integer, if possible.
 	y=int(b)
 	if x>y:
 		return x
