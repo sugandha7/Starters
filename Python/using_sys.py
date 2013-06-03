00de67b0d9b4c1cd50d6c609d37c269703104200
Ayush Goel <ayush.g@directi.com>
2013-06-03 11:25:18 +0530

    Form folder hierarchy

---
+++ Python/using_sys.py
@@ -0,0 +1,4 @@
+import sys
+for i in sys.argv:
+	print(i)                    # Prints the command line arguments entered.
+print('The PYTHONPATH is',sys.path) # Loooks for modules in directories listed in sys.path variable.
