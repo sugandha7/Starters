079a1d626a0f620eaf753c44d19295762e882725
Sugandha Goel <sugandhagoel7@gmail.com>
2013-06-04 15:25:36 +0530

    Comments updated.

--- Python/using_sys.py
+++ Python/using_sys.py
@@ -1,8 +1,9 @@
 import sys
 
 for i in sys.argv:
+    # Prints the command line arguments entered.
     print(i) 
-    # Prints the command line arguments entered.     
-                     
+    
+# Looks for modules in directories listed in sys.path variable.                   
 print('The PYTHONPATH is',sys.path) 
-# Prints the command line arguments entered.
+
