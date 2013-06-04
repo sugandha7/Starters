24e676cec847c71fba76b71b140e175159966c96
Sugandha Goel <sugandhagoel711@gmail.com>
2013-06-04 10:34:46 +0530

    Changed code according to pep8

--- Python/using_sys.py
+++ Python/using_sys.py
@@ -1,4 +1,8 @@
 import sys
+
 for i in sys.argv:
-	print(i)                    # Prints the command line arguments entered.
-print('The PYTHONPATH is',sys.path) # Loooks for modules in directories listed in sys.path variable.
+    print(i) 
+    # Prints the command line arguments entered.     
+                     
+print('The PYTHONPATH is',sys.path) 
+# Prints the command line arguments entered.
