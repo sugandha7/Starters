3c5a5380dfac4e6288748a04c2823e00f312f830
Sugandha Goel <sugandhagoel711@gmail.com>
2013-06-06 13:51:41 +0530

    Revert "Merge branch 'style-guide-edit' of https://github.com/sugandha7/Starters into style-guide-edit"
    This reverts commit 3d8de7e07e9a579b882f1d34af1a2d4c35fc4e4a, reversing
    changes made to 84f6b2e5cde33c554ba310294dd7455bc37a8964.

--- Python/using_sys.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-for i in sys.argv:
-    # Prints the command line arguments entered.
-    print(i) 
-    
-# Looks for modules in directories listed in sys.path variable.                   
-print('The PYTHONPATH is',sys.path) 
-
