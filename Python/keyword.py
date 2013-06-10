5522d0f7e272b790a44e69c1c2539487c07e820e
Sugandha Goel <sugandhagoel7@gmail.com>
2013-06-10 16:45:57 +0530

    Update keyword.py

--- Python/keyword.py
+++ Python/keyword.py
@@ -3,4 +3,4 @@ def func(a, b=5, c=10):  # b and c are default parameters
 
 func(1, 30)
 func(c=24, a=1000)  # a and c are keyword arguments
-func(23, c=7)   #c is a keyword argument
+func(23, c=7)   # c is a keyword argument
