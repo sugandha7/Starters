fcc19f61fb8eed447e2ccc16e785c64e602ba0ec
Sugandha Goel <sugandhagoel7@gmail.com>
2013-06-10 16:44:14 +0530

    Update printmax.py

--- Python/printmax.py
+++ Python/printmax.py
@@ -1,4 +1,4 @@
-def printmax(a,b):
+def printmax(a, b):
     ''' Prints the max of two numbers.
 	
     The numbers must be integers.
@@ -10,5 +10,5 @@ def printmax(a,b):
         return x
     else:
         return y
-print('The max of two numbers is', printmax(3,5))
+print('The max of two numbers is', printmax(3, 5))
 print(printmax.__doc__)
