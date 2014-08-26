9c91e581847de7ab3b183b4c8bd41594ff54989e
Sugandha Goel <sugandhagoel711@gmail.com>
2014-08-26 15:12:20 +0530

    Basic linked list operations implemented.

---
+++ C/data_structures/linked-list.c
@@ -0,0 +1,142 @@
+#include<stdio.h>
+struct node
+{
+	int data;
+	struct node* next;
+}*head;
+
+
+int main()
+{
+	int i, num;
+	head = NULL;
+	while(1)
+	{
+		printf("\nList Operations\n");
+		printf("===============\n");
+		printf("1.Insert\n");
+		printf("2.Display\n");
+		printf("3.Size\n");
+		printf("4.Delete\n");
+		printf("5.Exit\n");
+		printf("Enter your choice : ");
+		if(scanf("%d",&i)<=0){
+			printf("Enter only an Integer\n");
+			exit(0);
+		} 
+		else {
+			switch(i)
+			{
+				case 1:	printf("Enter the number to be inserted.\n");
+						scanf("%d", &num);
+						insert_element(num);
+						break;
+				case 2: display_list(head);
+						break;
+				case 3: size(head);
+						break;
+				case 4: printf("Enter the element to be deleted.\n");
+						scanf("%d", &num);
+						delete_element(num);
+						break;
+				case 5: return 0;
+				default: printf("Invalid Operation.\n");
+						 break;
+			}
+		}
+	}
+	return 0;
+}
+
+void insert_element(num)
+{
+	struct node  *temp, *ptr;
+	temp = (struct node*)malloc(sizeof(struct node));
+	temp->data = num;
+	temp->next = NULL;
+	if(head == NULL)
+	{
+		head = temp;
+	}
+	else
+	{
+		ptr = head;
+		while(ptr->next != NULL)
+		{
+			ptr = ptr->next;
+		}
+		ptr->next = temp;
+	}
+	return;
+}
+
+
+
+void display_list(head)
+{
+	struct node *temp = head;
+	if(head == NULL)
+	{
+		printf("List is empty.\n");
+	}
+	else
+	{
+		printf("The list is - \n");
+		while(temp != NULL)
+		{
+			printf("%d\n", temp->data);
+			temp = temp->next;
+		}
+	}
+	return;
+}
+
+void size(head)
+{
+	struct node *temp = head;
+	int count = 0;
+	while(temp != NULL)
+	{
+		count ++;
+		temp = temp->next;
+	}
+	printf("The size of the list is %d", count);
+	return;
+}
+
+void delete_element(num)
+{
+	struct node *temp, *ptr;
+	ptr = head;
+	if(head == NULL)
+	{
+		printf("Cannot Delte-->List is empty.\n");
+	}
+	else
+	{
+		if(ptr->data == num)
+		{
+			temp = ptr;
+			head = temp->next;
+			free(temp);
+			return;
+		}
+		else
+		{
+			while(ptr->next != NULL)
+			{
+				if(ptr->next->data == num)
+				{
+					temp = ptr->next;
+					ptr->next = temp->next;
+					free(temp);
+					return;
+				}
+				ptr = ptr->next;
+			}
+		}
+		printf("Invalid item.\n");
+	}
+	return;
+}
+
