a410763df44b622ab06f4963fc6725a73be8d985
Sugandha Goel <sugandhagoel711@gmail.com>
2014-08-26 15:05:52 +0530

    Exercises till exercise 10 completed.

--- C/exercises/ex3.c
+++ C/exercises/ex3.c
@@ -1,6 +1,12 @@
-#include<stdio.h>
-int main(int argc, char*argv[])
+#include <stdio.h>
+
+int main()
 {
-    puts("Hello world.");
-    return 0;
+	int age = 10;
+	int height = 72;
+
+	printf("I am %d years old.\n", age);
+	printf("I am %d inches tall.\n", height);
+
+	return 0;
 }
